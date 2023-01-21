0270769bbb8cb736c9aebe57ba402144e084d8c2
zisra <100528712+zisra@users.noreply.github.com>
2023-01-21 00:23:09 -0600

    Delete config.js

--- public/config.js
+++
@@ -1,7 +0,0 @@
-window.__CONFIG__ = {
-	// url must NOT end with a slash
-	VITE_CORS_PROXY_URL: 'https://proxy.defly.workers.dev',
-
-	VITE_TMDB_API_KEY: 'b030404650f279792a8d3287232358e3',
-	VITE_OMDB_API_KEY: 'aa0937c0',
-};
