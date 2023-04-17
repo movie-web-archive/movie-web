452982fb7709522f44be92f5122e64f86c4333c2
frost768 <droidplus99@gmail.com>
2023-04-17 14:36:29 +0300

    split media session logic into separate effects

--- src/video/components/internal/MediaSessionInternal.tsx
+++ src/video/components/internal/MediaSessionInternal.tsx
@@ -1,44 +1,89 @@
 import { MWMediaType } from "@/backend/metadata/types";
-import { getPlayerState } from "@/video/state/cache";
 import { useVideoPlayerDescriptor } from "@/video/state/hooks";
-import { useEffect } from "react";
+import { useControls } from "@/video/state/logic/controls";
+import { useProgress } from "@/video/state/logic/progress";
+import { useEffect, useRef } from "react";
+import { VideoMetaEvent, useMeta } from "@/video/state/logic/meta";
 
 export default function MediaSessionInternal() {
   const descriptor = useVideoPlayerDescriptor();
-  const state = getPlayerState(descriptor);
+  const metaRef = useRef<VideoMetaEvent>(useMeta(descriptor));
+  const controlsRef = useRef(useControls(descriptor));
+  const progressRef = useRef(useProgress(descriptor));
+  const mediaSessionRef = useRef<MediaSession | null>(null);
+
+  // useEffect(() => {
+  //   controlsRef.current = controls;
+  //   progressRef.current = progress;
+  //   metaRef.current = metadata;
+  // }, [progress, controls, metadata]);
+
   useEffect(() => {
     if (!("mediaSession" in navigator)) return;
-    const stateProvider = state.stateProvider;
-    if (!stateProvider) return;
-    if (!state.meta) return;
+    mediaSessionRef.current = navigator.mediaSession;
+    const controlsRefCurrent = controlsRef.current;
+    const progressRefCurrent = progressRef.current;
+    const mediaSession = mediaSessionRef.current;
+    mediaSession.setActionHandler("play", () => controlsRefCurrent.play());
+    mediaSession.setActionHandler("pause", () => controlsRefCurrent.pause());
+    const seekTime = 20; // this could be a setting
+    mediaSession.setActionHandler("seekbackward", () => {
+      if (progressRefCurrent.time < seekTime) {
+        controlsRefCurrent.setTime(0);
+      } else {
+        controlsRefCurrent.setTime(progressRefCurrent.time - seekTime);
+      }
+    });
+
+    mediaSession.setActionHandler("seekforward", () => {
+      if (progressRefCurrent.time + seekTime > progressRefCurrent.duration) {
+        controlsRefCurrent.setTime(progressRefCurrent.duration);
+      } else {
+        controlsRefCurrent.setTime(progressRefCurrent.time + seekTime);
+      }
+    });
+    return () => {
+      // mediaSession.metadata = null;
+      mediaSession.playbackState = "none";
+      mediaSession.setActionHandler("play", null);
+      mediaSession.setActionHandler("pause", null);
+      mediaSession.setActionHandler("seekbackward", null);
+      mediaSession.setActionHandler("seekforward", null);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (mediaSessionRef.current === null) return;
+    if (!metaRef.current) return;
+    const media = metaRef.current.meta.meta;
     let artwork: any[] = [];
-    if (state.meta.meta.meta.poster) {
+    if (media.poster) {
       artwork = [
         // for low end devices
         {
-          src: state.meta.meta.meta.poster,
+          src: media.poster,
           sizes: "256x256",
           type: "image/png",
         },
         // for high end devices
         {
-          src: state.meta.meta.meta.poster,
+          src: media.poster,
           sizes: "512x512",
           type: "image/png",
         },
       ];
     }
-    const mediaSession = navigator.mediaSession;
+    const mediaSession = mediaSessionRef.current;
     mediaSession.metadata = new MediaMetadata({ artwork });
-    if (state.meta.meta.meta.type === MWMediaType.MOVIE) {
-      mediaSession.metadata.title = state.meta.meta.meta.title;
-      if (state.meta.meta.meta.year) {
-        mediaSession.metadata.artist = state.meta.meta.meta.year;
+    if (media.type === MWMediaType.MOVIE) {
+      mediaSession.metadata.title = media.title;
+      if (media.year) {
+        mediaSession.metadata.artist = media.year;
       }
     }
-    if (state.meta.meta.meta.type === MWMediaType.SERIES) {
-      const currentEpisodeId = state.meta.episode?.episodeId;
-      const currentSeason = state.meta.meta.meta.seasonData;
+    if (media.type === MWMediaType.SERIES) {
+      const currentEpisodeId = metaRef.current.episode?.episodeId;
+      const currentSeason = media.seasonData;
       const currentEpisode = currentSeason.episodes.find(
         (episode) => episode.id === currentEpisodeId
       );
@@ -46,35 +91,9 @@ export default function MediaSessionInternal() {
         mediaSession.metadata.title = currentEpisode.title;
       }
       mediaSession.metadata.artist = currentSeason.title;
-      mediaSession.metadata.album = state.meta.meta.meta.title;
+      mediaSession.metadata.album = media.title;
     }
-    mediaSession.setActionHandler("play", () => stateProvider.play());
-    mediaSession.setActionHandler("pause", () => stateProvider.pause());
-    const seekTime = 20; // this could be a setting
-    mediaSession.setActionHandler("seekbackward", () => {
-      if (state.progress.time < seekTime) {
-        stateProvider.setTime(0);
-      } else {
-        stateProvider.setTime(state.progress.time - seekTime);
-      }
-    });
+  });
 
-    mediaSession.setActionHandler("seekforward", () => {
-      if (state.progress.time + seekTime > state.progress.duration) {
-        stateProvider.setTime(state.progress.duration);
-      } else {
-        stateProvider.setTime(state.progress.time + seekTime);
-      }
-    });
-    return () => {
-      mediaSession.metadata = null;
-      mediaSession.playbackState = "none";
-      mediaSession.setActionHandler("play", null);
-      mediaSession.setActionHandler("pause", null);
-      mediaSession.setActionHandler("seekbackward", null);
-      mediaSession.setActionHandler("seekforward", null);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [state.stateProvider]);
   return null;
 }
