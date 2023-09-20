d9591d77fb5bec42b58b9253a18bbabdedc83968
InstaTechHD <68559185+InstaTechHD@users.noreply.github.com>
2023-09-20 12:13:30 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -35,8 +35,8 @@ export default defineConfig({
         "safari-pinned-tab.svg",
       ],
       manifest: {
-        name: "movie-web",
-        short_name: "movie-web",
+        name: "zflixer",
+        short_name: "zflixer",
         description: "The place for your favourite movies & shows",
         theme_color: "#120f1d",
         background_color: "#120f1d",
