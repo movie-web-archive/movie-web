509a974947221e21d47b10e225114b48487be6f3
Robert Jarrett <howtotech77@gmail.com>
2023-10-01 22:08:04 -0600

    Update constants.ts

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,5 +1,5 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
-export const DISCORD_LINK = "https://discord.gg/Jhqt4Xzpfb";
+export const DISCORD_LINK = "#";
 export const GITHUB_LINK = "#";
 export const GA_ID = "#";
 export const SENTRY_DSN =
