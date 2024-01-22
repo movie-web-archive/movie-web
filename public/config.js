342ccaa80faa7960de4318d87096e7334f74d0f6
John1414141 <j14324@mail.i-dork.com>
2024-01-22 14:32:30 +0000

    Pending changes exported from your codespace

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "",
-  VITE_TMDB_READ_API_KEY: ""
+  VITE_CORS_PROXY_URL: "https://worker-shiny-king-e4a7.j14324.workers.dev",
+  VITE_TMDB_READ_API_KEY: "1569678a3fd7f699bb647a6607de2e2a"
 };
