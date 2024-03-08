58290551124c94a20bf5e181909ec88f471d29af
Atharva Rane <atharvarane051102@gmail.com>
2024-03-08 18:47:03 +0530

    setup local config

--- public/config.js
+++ public/config.js
@@ -1,10 +1,8 @@
 window.__CONFIG__ = {
-  // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "",
-
-  // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "",
+  VITE_CORS_PROXY_URL: 'https://playful-starlight-f6766e.netlify.app',
+  VITE_TMDB_READ_API_KEY:
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MWQ4OTYyYWNiNzY4OWVhMDcwZjEzOTZhZTM1MmFjZSIsInN1YiI6IjY1YjE0NmI3OTljOTY0MDE5MzFmYTZkZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qPjn4YWG_RlXVTpFIwBKH3CTViOhpqy7zm2oHkqRl9A',
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
@@ -16,5 +14,5 @@ window.__CONFIG__ = {
   VITE_BACKEND_URL: null,
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
-  VITE_DISALLOWED_IDS: "",
+  VITE_DISALLOWED_IDS: '',
 };
